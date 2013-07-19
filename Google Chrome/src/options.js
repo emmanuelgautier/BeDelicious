a122f7968a1b4bdd97990ddb79cc3bf1e2b00ab9
emmanuelgautier <manu.gautier1394@gmail.com>
2013-07-19 10:46:29 +0200

    Correction encodage

--- Google Chrome/src/options.js
+++ Google Chrome/src/options.js
@@ -2,30 +2,30 @@ var reminder = localStorage.reminderme;
 
 function save_options() {
 	var delay 		= document.querySelector("#delay").value;
-	
+
 	var status	= "";
-	
+
 	//on type la variable comme entier
 	delay = parseInt(delay);
-	
-	//on vérifie la valeur du champ
+
+	//on vÃ©rifie la valeur du champ
 	if(delay <= 0)
 		status = '<font color="red">Erreur de sauvegarde !</font>';
 	else if(delay < 2 || delay >= 30)
-		status = '<font color="red">Le délai doit être compris entre 2 et 30 minutes !</font>';
+		status = '<font color="red">Le dÃ©lai doit Ãªtre compris entre 2 et 30 minutes !</font>';
 	else {
 		if(reminder)
 		{
 			var hour 	= document.querySelector("#reminderHour").value;
 			var minutes	= document.querySelector("#reminderMinutes").value;
-			
+
 			hour 	= parseInt(hour);
 			minutes	= parseInt(minutes);
-			
+
 			if(hour < 0 || hour > 23)
-				status = '<font color="red">La valeur de l\'heure doit être comprise entre 0 et 24</font>';
+				status = '<font color="red">La valeur de l\'heure doit Ãªtre comprise entre 0 et 24</font>';
 			else if(minutes < 0 || minutes > 59)
-				status = '<font color="red">La valeurs des minutes doit être compris entre 0 et 60</font>';
+				status = '<font color="red">La valeurs des minutes doit Ãªtre compris entre 0 et 60</font>';
 			else
 			{
 				localStorage.reminderme = true;
@@ -39,12 +39,12 @@ function save_options() {
 			localStorage.hour		= 0;
 			localStorage.minutes	= 0;
 		}
-		
+
 		localStorage.delay = delay;
-		
-		if(!status) status	= '<font color="green">Options Sauvegardées !</font>';
+
+		if(!status) status	= '<font color="green">Options SauvegardÃ©es !</font>';
 	}
-	
+
 	document.querySelector("#status").innerHTML = status;
 	setTimeout(function() { document.querySelector("#status").innerHTML = ""; }, 1500);
 }
